f25a6918e68133f6ec3c1f51e22762dc87a820ed
Rahul U S <s.rahus@gmail.com>
2025-04-12 01:06:58 +0530

    update feature

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,7 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello DevOps World!");
+        System.out.println("This is Assignment1!");
     }
 }
